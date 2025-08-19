9701622722aca8412ca5fdf5e8fe1d37e42ae7df
Arathy <arathyrajeesh2@gmail.com>
2025-08-19 05:34:33 +0530

    use reducer add

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,25 +1,30 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
-function App() {
-  const [count,setCount]=useState(0);
-  function increment() {
-    setCount(count + 1);
-  };
-  function decrement() {
-    setCount(count - 1);
-  };
-  function resetCounter() {
-    setCount(0); 
-  };
-  return(
-    <div class="container">
-    <h1>Count: {count}</h1>
-    <button onClick={increment}>+</button>
-    <button onClick={resetCounter}>Reset</button>
+import React, { useReducer } from 'react';
 
-    <button onClick={decrement}>-</button>
-  </div>
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return 0;
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div className="container">
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
   );
 }
+
 export default App;
